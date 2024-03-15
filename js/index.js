77b56c30793043e3b8f76695d732a1b764b88424
Bobur Makhmadkulov <boburmakhmadkulov05@gmail.com>
2024-03-15 10:03:47 +0500

    fifth commit

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ let showFour = document.querySelector('#showFour')
 let showFull = document.querySelector('#showFull')
 let busket = document.querySelector('#busket')
 let busket_menu = document.querySelector('#busket-menu')
+let close_busket = document.querySelector('.menu_title button')
 
 reload(arr, container)
 
@@ -18,3 +19,17 @@ showFull.onclick = () => {
 busket.onclick = () => {
     busket_menu.style.right = '0'
 }
+busket_menu.onmouseenter = () => {
+    setTimeout(function() {
+        close_busket.style.backgroundImage = 'url(../icons/close.svg)'
+    }, 200)
+}
+busket_menu.onmouseleave = () => {
+    setTimeout(function() {
+        close_busket.style.backgroundImage = ''
+    }, 0)
+}
+close_busket.onclick = () => {
+    busket_menu.style.right = '-50%'
+}
+
