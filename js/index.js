fdc18ab713b8dd2a7c8a12129ed657dcea5f096d
Bobur Makhmadkulov <boburmakhmadkulov05@gmail.com>
2024-03-16 22:35:55 +0500

    seventh commit

--- js/index.js
+++ js/index.js
@@ -24,47 +24,55 @@ close_busket.onclick = () => {
     busket_menu.style.right = '-530px'
 }
 busket_menu.onmouseenter = () => {
-    setTimeout(function() {
+    setTimeout(function () {
         close_busket.style.backgroundImage = 'url(../icons/close.svg)'
     }, 200)
 }
 busket_menu.onmouseleave = () => {
-    setTimeout(function() {
+    setTimeout(function () {
         close_busket.style.backgroundImage = ''
     }, 0)
 }
 
-let item = document.createElement('div')
-let img_div = document.createElement('div')
-let item_img = document.createElement('img')
-let item_staff = document.createElement('div')
-let i_cot = document.createElement('span')
-let i_def = document.createElement('span')
-let item_counter = document.createElement('div')
-let item_amount = document.createElement('span')
-let item_plus = document.createElement('button')
-let item_minus = document.createElement('button')
+function reloadMenu(arr, place) {
+    place.innerHTML = ''
 
-item.classList.add('item')
-img_div.classList.add('img_div')
-item_img.classList.add('item_img')
-item_staff.classList.add('item_staff')
-i_cot.classList.add('i_cot')
-i_def.classList.add('i_def')
-item_counter.classList.add('item_counter')
-item_amount.classList.add('apm')
-item_plus.classList.add('apm')
-item_minus.classList.add('apm')
+    for (let item of arr) {
+        let elem = document.createElement('div')
+        let img_div = document.createElement('div')
+        let elem_img = document.createElement('img')
+        let elem_staff = document.createElement('div')
+        let i_cot = document.createElement('span')
+        let i_def = document.createElement('span')
+        let elem_counter = document.createElement('div')
+        let elem_amount = document.createElement('span')
+        let elem_plus = document.createElement('button')
+        let elem_minus = document.createElement('button')
 
-item_img.src = 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg'
-i_cot.innerHTML = "men's clothing"
-i_def.innerHTML = 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops'
-item_amount.innerHTML = '0'
-item_plus.innerHTML = '+'
-item_minus.innerHTML = '-'
+        elem.classList.add('item')
+        img_div.classList.add('img_div')
+        elem_img.classList.add('item_img')
+        elem_staff.classList.add('item_staff')
+        i_cot.classList.add('i_cot')
+        i_def.classList.add('i_def')
+        elem_counter.classList.add('item_counter')
+        elem_amount.classList.add('apm')
+        elem_plus.classList.add('apm')
+        elem_minus.classList.add('apm')
 
-menu_container.append(item)
-item.append(img_div, item_staff, item_counter)
-img_div.append(item_img)
-item_staff.append(i_cot, i_def)
-item_counter.append(item_plus, item_amount, item_minus)
+        elem_img.src = item.image
+        i_cot.innerHTML = item.category
+        i_def.innerHTML = item.title
+        elem_amount.innerHTML = '0'
+        elem_plus.innerHTML = '+'
+        elem_minus.innerHTML = '-'
+
+        place.append(elem)
+        elem.append(img_div, elem_staff, elem_counter)
+        img_div.append(elem_img)
+        elem_staff.append(i_cot, i_def)
+        elem_counter.append(elem_plus, elem_amount, elem_minus)
+    }
+}
+
+reloadMenu(arr, menu_container)
